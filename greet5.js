86e9b57d1d53891c34f3dc177075b53a711a9f25
20186935 BRANDON ALEXANDER MORAN VAZQUEZ <20186935@TELEMATICA.UCOL.MX>
2023-02-21 08:10:12 -0600

    Extra functions and greet5

--- greet5.js
+++ greet5.js
@@ -1,8 +1,13 @@
 let greeting = "Hello from greet5";
 
-let greet =()=>{
-    console.log(greeting);
-}
+let greet =()=>console.log("I'm saying hi");
+let jump =()=>console.log("I'm jumping");
+let run =()=>console.log("I'm running");
+let talk = ()=>console.log("I'm talking");
+
 module.exports = {
-    greet: greet
+    greet,
+    jump,
+    run,
+    talk
 }
